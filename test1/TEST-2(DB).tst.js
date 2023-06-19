4be1884261cc04934fc2250c5cd1fabd45b3d394
Alex <gglodessa@gmail.com>
2023-06-19 17:23:31 +0300

    fix db

--- test1/TEST-2(DB).tst.js
+++ test1/TEST-2(DB).tst.js
@@ -1,40 +1,50 @@
 "use strict"
 const sqlite3 = require('sqlite3').verbose();
 const logger = require('winston');
+const assert = require('chai').assert;
 
 async function testTwo() {
 
   try {
 
-    logger.info('1.database connection');
-let db = new sqlite3.Database('c:\\Users\\olexiy.klein\\JSAutomation\\JSTests\\Countries.db', sqlite3.OPEN_READWRITE, (err) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log('Connected database =>');
-  });
+    logger.info('1. Database connection ang get data');
+    const db = new sqlite3.Database('C:\\Users\\Alekseii\\js\\Cypress\\Countries.db');
+    const query = "SELECT * FROM Countries";
 
-let query = "SELECT SUM(Population) FROM Countries WHERE Population < 2000000000";
+    db.all(query, (err, rows) => {
+      if (err) {
+        console.error(err.message);
+        return;
+      }
 
-logger.info('1. The total population of all 4 countries is less than 2 billion people');
-db.all(query, (err, result) =>{
-    console.log(err);
-    console.log(result);
-});
+      logger.info('2. Checking population density below 50 people/sq.km and Checking total population less than 2 billion people.');
+      let totalPopulation = 0;
+      let isUSADensityBelow50 = false;
 
-logger.info('1. Population density below 50 people/sq.km.');
+      rows.forEach((row) => {
+        const country = row.Country;
+        const population = row.Population;
+        const area = row.Area;
+        const density = population / area;
 
-let query2 = "SELECT Country FROM Countries  WHERE Population/Area < 50";
+        totalPopulation += population;
 
-db.all(query2, (err, result) =>{
-    console.log(err);
-    console.log(result);
-});  
+        if (country === 'USA' && density < 50) {
+          isUSADensityBelow50 = true;
+        }
+      });
 
-} finally {
-}
+      const isTotalPopulationLessThan2Billion = totalPopulation < 2000000000;
+      assert.isTrue(isUSADensityBelow50, 'The population density is not below 50 people/sq.km. in the USA');
+      assert.isTrue(isTotalPopulationLessThan2Billion, 'The total population of all 4 countries is not less than 2 billion people');
+
+      logger.info('3. Closing database connection');
+      db.close();
+    });
+
+  } finally {
+  }
 };
-
 
 testTwo();
 
